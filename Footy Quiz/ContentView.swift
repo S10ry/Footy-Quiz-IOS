624cb2110706d738e29f408cd1f04a499679d944
Sory Diarra <sory@kreezalid.co>
2020-04-30 14:34:29 +0200

    Initial Commit

---
+++ Footy Quiz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Footy Quiz
+//
+//  Created by Sory Diarra on 30/04/2020.
+//  Copyright © 2020 Sory Diarra. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Footy QuizTests/Footy_QuizTests.swift
@@ -0,0 +1,34 @@
+//
+//  Footy_QuizTests.swift
+//  Footy QuizTests
+//
+//  Created by Sory Diarra on 30/04/2020.
+//  Copyright © 2020 Sory Diarra. All rights reserved.
+//
+
+import XCTest
+@testable import Footy_Quiz
+
+class Footy_QuizTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
